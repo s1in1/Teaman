602958ae6036e596bdbe3fca88f9a6e43d558f89
s1in1 <mirgalimovk06@gmail.com>
2026-01-12 01:02:56 +0300

    12/01

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $connection = new mysqli('mysql-8.4', 'root', '', 'teaman');
-    if ($connection->connect_error) {
-        die('DB connection error: '. $mysqli->connect_error);
-    }
-?>
